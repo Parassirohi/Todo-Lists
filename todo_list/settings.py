1f54d38ad69ebac102cf1f082e10c7b2f6d8543d
Paras Sirohi <parassirohi000@gmail.com>
2023-05-31 23:36:04 +0530

    Todo-List Project

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=#dc*zvz$jo$!m%pew1il+)v4ki(kp691%a)mhmmj7f%q08xz%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.90.239.51']
 
 
 # Application definition
